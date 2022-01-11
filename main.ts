42bca3300766e66c5fe020a01c34dd5bff935e51
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-01-11 10:46:12 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Vehicle = SpriteKind.create()
+    export const orb = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Vehicle, function (sprite, otherSprite) {
     if (Type == person) {
@@ -16,6 +17,12 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         pause(500)
         Type = person
     }
+    if (state == 2 && Type == vtype) {
+        effects.clouds.endScreenEffect()
+        tiles.setTilemap(tilemap`space`)
+        pause(2000)
+        startPlanet()
+    }
 })
 function createHangar () {
     XWing = sprites.create(assets.image`myImage`, SpriteKind.Vehicle)
@@ -27,9 +34,37 @@ function createHangar () {
     scene.setBackgroundColor(11)
     tiles.setTilemap(tilemap`hangar1`)
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (state != 0) {
+        laserBlast()
+    }
+})
+function startPlanet () {
+    newplanet = sprites.create(assets.image`planet`, SpriteKind.orb)
+    newplanet.setPosition(116, 91)
+    newplanet.setVelocity(randint(-50, 50), randint(-50, 50))
+    newplanet.setBounceOnWall(true)
+    planetNOW = 1
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.orb, function (sprite, otherSprite) {
+    state = 2
+    newplanet.destroy()
+    scene.setBackgroundColor(9)
+    effects.clouds.startScreenEffect()
+    tiles.setTilemap(tilemap`planetscape1`)
+})
+function laserBlast () {
+    zap = sprites.create(assets.image`laser`, SpriteKind.Projectile)
+    zap.setPosition(Luke.x, Luke.y)
+    zap.setVelocity(200, 0)
+    zap.setFlag(SpriteFlag.DestroyOnWall, true)
+}
+let zap: Sprite = null
+let newplanet: Sprite = null
 let YWing: Sprite = null
 let XWing: Sprite = null
 let veh: Sprite = null
+let planetNOW = 0
 let state = 0
 let Type = 0
 let Luke: Sprite = null
@@ -40,16 +75,29 @@ vtype = 3
 Luke = sprites.create(assets.image`Pilot`, SpriteKind.Player)
 Type = person
 state = 0
+planetNOW = 0
 createHangar()
 forever(function () {
-    if (state == 0) {
-        if (40 > Luke.x || 290 < Luke.x) {
+    if (40 > Luke.x || 250 < Luke.x) {
+        if (state == 0) {
             if (vtype == Type) {
                 state = 1
                 tiles.setTilemap(tilemap`space`)
                 YWing.destroy()
                 XWing.destroy()
             }
+        } else {
+            if (200 < Luke.x) {
+                Luke.x = 50
+            } else {
+                Luke.x = 200
+            }
         }
+    }
+})
+forever(function () {
+    if (planetNOW == 0 && 1 == state) {
+        pause(5000)
+        startPlanet()
     }
 })
